ad76a6d828ebefe8bc85d6e6bcc73ed4aa2dde5c
Robert-Rino <a901002666@gmail.com>
2017-02-23 21:38:45 +0800

    compose route

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require_relative 'config/environments'
 require_relative 'models/init'
 
 # Configuration Sharing Web Service
-class ShareConfigurationsAPI < Sinatra::Base
+class LunchHelperAPI < Sinatra::Base
   before do
     host_url = "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
     @request_url = URI.join(host_url, request.path.to_s)
@@ -18,95 +18,94 @@ class ShareConfigurationsAPI < Sinatra::Base
     # TODO: show all routes as json with links
   end
 
-  get '/api/v1/projects/?' do
-    content_type 'application/json'
+  # get '/api/v1/projects/?' do
+  #   content_type 'application/json'
+  #
+  #   JSON.pretty_generate(data: Project.all)
+  # end
 
-    JSON.pretty_generate(data: Project.all)
-  end
-
-  get '/api/v1/projects/:id' do
+  get '/api/v1/restaurants/:id' do
     content_type 'application/json'
 
     id = params[:id]
-    project = Project[id]
-    configurations = project ? Project[id].configurations : []
+    restaurant = Restaurant[id]
 
-    if project
-      JSON.pretty_generate(data: project, relationships: configurations)
+    if restaurant
+      JSON.pretty_generate(data: restaurant)
     else
       halt 404, "PROJECT NOT FOUND: #{id}"
     end
   end
 
-  post '/api/v1/projects/?' do
+  post '/api/v1/restaurants/?' do
     begin
       new_data = JSON.parse(request.body.read)
-      saved_project = Project.create(new_data)
+      saved_restaurant = Restaurant.create(new_data)
     rescue => e
       logger.info "FAILED to create new project: #{e.inspect}"
       halt 400
     end
 
-    new_location = URI.join(@request_url.to_s + '/', saved_project.id.to_s).to_s
+    new_location = URI.join(@request_url.to_s + '/', saved_restaurant.id.to_s).to_s
 
     status 201
     headers('Location' => new_location)
   end
 
-  get '/api/v1/projects/:id/configurations/?' do
-    content_type 'application/json'
-
-    project = Project[params[:id]]
-
-    JSON.pretty_generate(data: project.configurations)
-  end
-
-  get '/api/v1/projects/:project_id/configurations/:id/?' do
-    content_type 'application/json'
-
-    begin
-      doc_url = URI.join(@request_url.to_s + '/', 'document')
-      configuration = Configuration
-                      .where(project_id: params[:project_id], id: params[:id])
-                      .first
-      halt(404, 'Configuration not found') unless configuration
-      JSON.pretty_generate(data: {
-                             configuration: configuration,
-                             links: { document: doc_url }
-                           })
-    rescue => e
-      status 400
-      logger.info "FAILED to process GET configuration request: #{e.inspect}"
-      e.inspect
-    end
-  end
-
-  get '/api/v1/projects/:project_id/configurations/:id/document' do
-    content_type 'text/plain'
-
-    begin
-      Configuration
-        .where(project_id: params[:project_id], id: params[:id])
-        .first
-        .document
-    rescue => e
-      status 404
-      e.inspect
-    end
-  end
-
-  post '/api/v1/projects/:project_id/configurations/?' do
+  # get '/api/v1/projects/:id/configurations/?' do
+  #   content_type 'application/json'
+  #
+  #   project = Project[params[:id]]
+  #
+  #   JSON.pretty_generate(data: project.configurations)
+  # end
+  #
+  # get '/api/v1/projects/:project_id/configurations/:id/?' do
+  #   content_type 'application/json'
+  #
+  #   begin
+  #     doc_url = URI.join(@request_url.to_s + '/', 'document')
+  #     configuration = Configuration
+  #                     .where(project_id: params[:project_id], id: params[:id])
+  #                     .first
+  #     halt(404, 'Configuration not found') unless configuration
+  #     JSON.pretty_generate(data: {
+  #                            configuration: configuration,
+  #                            links: { document: doc_url }
+  #                          })
+  #   rescue => e
+  #     status 400
+  #     logger.info "FAILED to process GET configuration request: #{e.inspect}"
+  #     e.inspect
+  #   end
+  # end
+  #
+  # get '/api/v1/projects/:project_id/configurations/:id/document' do
+  #   content_type 'text/plain'
+  #
+  #   begin
+  #     Configuration
+  #       .where(project_id: params[:project_id], id: params[:id])
+  #       .first
+  #       .document
+  #   rescue => e
+  #     status 404
+  #     e.inspect
+  #   end
+  # end
+  #
+  post '/api/v1/projects/:restaurant_id/dishs/?' do
     begin
       new_data = JSON.parse(request.body.read)
-      project = Project[params[:project_id]]
-      saved_config = project.add_configuration(new_data)
+      restaurant = Restaurant[params[:restaurant_id]]
+      saved_dish = restaurant.add_dish(new_data)
     rescue => e
       logger.info "FAILED to create new config: #{e.inspect}"
       halt 400
     end
 
     status 201
-    new_location = URI.join(@request_url.to_s + '/', saved_config.id.to_s).to_s
+    new_location = URI.join(@request_url.to_s + '/', saved_dish.id.to_s).to_s
     headers('Location' => new_location)
   end
 end
