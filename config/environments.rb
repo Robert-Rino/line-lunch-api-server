3b9cacdd2ea24a693f108426f506c03b366942e7
Robert-Rino <a901002666@gmail.com>
2017-02-28 18:34:13 +0800

    environment.rb

--- config/environments.rb
+++ config/environments.rb
@@ -8,14 +8,13 @@ configure :test do
   ENV['DATABASE_URL'] = 'sqlite://db/test.db'
 end
 
+configure :production do
+  # ENV['DATABASE_URL'] should be set by Heroku
+  # Configuration settings should be directly set on Heroku
+end
+
 configure do
   enable :logging
   require 'sequel'
   DB = Sequel.connect(ENV['DATABASE_URL'])
-end
-
-
-configure :production do
-  # ENV['DATABASE_URL'] should be set by Heroku
-  # Configuration settings should be directly set on Heroku
 end
